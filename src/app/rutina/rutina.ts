481ace0fd8594905001aed776314b3e693c2a3d1
Lara <the.lemonetti@gmail.com>
2023-02-25 21:50:06 -0800

    Se añade lista de ejercicios a la vista de detalle de rutina

--- src/app/rutina/rutina.ts
+++ src/app/rutina/rutina.ts
@@ -1,13 +1,21 @@
+import { Ejercicio } from "../ejercicio/ejercicio";
+import { Entrenamiento } from "../entrenamiento/entrenamiento";
 
 export class Rutina {
 
   id: number;
   nombre: string;
   descripcion: string;
+  entrenamientos: Array<Entrenamiento>
+  ejercicioRutina: Array<Ejercicio>
 
-  public constructor(id: number, nombre: string, descripcion:string) {
+
+  public constructor(id: number, nombre: string, descripcion:string,
+    entrenamientos:Array<Entrenamiento>, ejercicioRutina:Array<Ejercicio>) {
     this.id = id;
     this.nombre = nombre;
     this.descripcion = descripcion;
+    this.entrenamientos = entrenamientos;
+    this.ejercicioRutina = ejercicioRutina;
   }
 }
