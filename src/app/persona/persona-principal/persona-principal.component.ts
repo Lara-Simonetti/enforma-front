797dab29bfb33ae14f2fb7fdc0e8f96792aed887
s-cortes <s.cortes@uniandes.edu.co>
2023-03-01 22:44:11 -0500

    Lógica para Navegar al reporte

--- src/app/persona/persona-principal/persona-principal.component.ts
+++ src/app/persona/persona-principal/persona-principal.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { PersonaService } from '../persona.service';
 import { Persona } from '../persona';
-import { catchError } from 'rxjs';
+import { catchError, of } from 'rxjs';
 
 @Component({
   selector: 'app-persona-principal',
@@ -26,12 +26,21 @@ export class PersonaPrincipalComponent implements OnInit {
   detallePersona(): void {
     const idUsuario = parseInt(sessionStorage.getItem('idUsuario'));
     if (idUsuario) {
-      this.personaService.darPersona(idUsuario).subscribe((persona) => {
-        console.log(persona);
-        this.persona = persona;
-      });
+      this.personaService.darPersona(idUsuario)
+        .pipe(
+          catchError((error) => {
+            return of(null);
+          })
+        ).subscribe((persona) => {
+          if(persona) this.persona = persona;
+          else this.routerPath.navigate(['']);
+        });
     } else {
       this.routerPath.navigate(['']);
     }
+  }
+
+  personaReporte(idPersona: number): void {
+    this.routerPath.navigate(['/persona/reporte/' + idPersona]);
   }
 }
