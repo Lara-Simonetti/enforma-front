e22485a2dd52b69c3044e3447e9254c92192032c
s-cortes <s.cortes@uniandes.edu.co>
2023-03-09 22:29:57 -0500

    Validación de rol en vista de reporte

--- src/app/persona/persona-reporte/persona-reporte.component.ts
+++ src/app/persona/persona-reporte/persona-reporte.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Persona } from '../persona';
 import { PersonaService } from '../persona.service';
+import { ToastrService } from 'ngx-toastr';
+import { ROLES } from 'src/app/roles';
 
 @Component({
   selector: 'app-persona-reporte',
@@ -18,11 +20,28 @@ export class PersonaReporteComponent implements OnInit {
   constructor(
     private routerPath: Router,
     private router: ActivatedRoute,
+    private toastr: ToastrService,
     private personaService: PersonaService
   ) { }
 
   ngOnInit() {
-    const idPersona = parseInt(this.router.snapshot.params['id']);
+    const rol = sessionStorage.getItem('rol');
+    if (rol === ROLES.entrenador || rol === ROLES.persona) {
+      const idPersona = parseInt(this.router.snapshot.params['id']);
+      if (idPersona) {
+        this.darReporte(idPersona);
+      } else {
+        this.toastr.error('Error', 'No se ha encontrado al usuario');
+        this.routerPath.navigate(['']);
+      }
+    } else {
+      this.toastr.error('Error', 'El Usuario no está autorizado');
+      this.routerPath.navigate(['']);
+    }
+
+  }
+
+  darReporte(idPersona: number): void {
     this.personaService.darReporte(idPersona).subscribe((reporte) => {
       console.log(reporte)
       this.persona = reporte.persona
@@ -36,6 +55,4 @@ export class PersonaReporteComponent implements OnInit {
     const idPersona = parseInt(this.router.snapshot.params['id']);
     this.routerPath.navigate(['/persona/' + idPersona]);
   }
-
-
 }
