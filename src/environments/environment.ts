8d17c694309e35e8f367be0387c421b1bb69306e
s-cortes <s.cortes@uniandes.edu.co>
2023-03-12 07:35:55 -0500

    url backedn

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,4 @@
 export const environment = {
     production: false,
-//    apiUrl: 'https://enforma-15-back.herokuapp.com/'
-apiUrl: 'http://127.0.0.1:5000/'
+    apiUrl: 'https://enforma-15-back.herokuapp.com/'
   };
